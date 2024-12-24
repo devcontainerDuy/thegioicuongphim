39811fbf58124725d6be01b4561406aaa54f76a6
devcontainerDuy <khanhduytran1803@gmail.com>
2024-12-24 18:12:30 +0700

    Refactor Home component to improve readability of product listings

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,6 +29,7 @@ function Home() {
         console.log(err);
       });
   }, []);
+
   return (
     <>
       <Header />
@@ -36,11 +37,23 @@ function Home() {
       <main className="py-3">
         <section className="container">
           <h3 className="py-2 text-danger border-bottom border-danger fw-bold">Phim mới cập nhật</h3>
-          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 g-2">{products.length > 0 ? products.map((p, i) => <Product key={i} name={p.name} slug={p.slug} image={p.thumb_url} totalEpisodes={p.total_episodes} currentEpisode={p.current_episode} time={p.time} />) : <p className="text-danger">Không có phim nào được tìm thấy.</p>}</div>
+          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 g-2">
+            {products.length > 0 ? (
+              products.map((p, i) => <Product key={i} name={p.name} slug={p.slug} image={p.thumb_url} totalEpisodes={p.total_episodes} currentEpisode={p.current_episode} time={p.time} />)
+            ) : (
+              <p className="text-danger">Không có phim nào được tìm thấy.</p>
+            )}
+          </div>
         </section>
         <section className="container py-3">
           <h3 className="py-2 text-danger border-bottom border-danger">Phim Việt Nam</h3>
-          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 g-2">{vietnam.length > 0 ? vietnam.map((p, i) => <Product key={i} name={p.name} slug={p.slug} image={p.thumb_url} totalEpisodes={p.total_episodes} currentEpisode={p.current_episode} time={p.time} />) : <p>Không có phim nào được tìm thấy.</p>}</div>
+          <div className="row row-cols-2 row-cols-sm-3 row-cols-md-4 row-cols-lg-5 g-2">
+            {vietnam.length > 0 ? (
+              vietnam.map((p, i) => <Product key={i} name={p.name} slug={p.slug} image={p.thumb_url} totalEpisodes={p.total_episodes} currentEpisode={p.current_episode} time={p.time} />)
+            ) : (
+              <p>Không có phim nào được tìm thấy.</p>
+            )}
+          </div>
         </section>
       </main>
 
