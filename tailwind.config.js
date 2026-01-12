cda077b287167e5a7c43b2371cc976f02a256054
devcontainerDuy <khanhduytran1803@gmail.com>
2026-01-13 00:26:18 +0700

    feat(prisma): upgrade to Prisma 7 with custom output config

--- tailwind.config.js
+++
@@ -1,3 +0,0 @@
-// Workaround for Tailwind IntelliSense in monorepo
-// This file helps VSCode Tailwind extension resolve the correct config
-module.exports = require('./client/tailwind.config.js');
