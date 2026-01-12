deaa44c2ef100a6692059ce100abb75a162428e3
devcontainerDuy <khanhduytran1803@gmail.com>
2026-01-12 09:42:17 +0700

    fix: configure Tailwind IntelliSense for monorepo
    - Add VSCode settings to point Tailwind extension to client config
    - Exclude server files from Tailwind processing
    - Create root tailwind.config.js workaround for extension
    - Add experimental classRegex for cva() and cn() functions
    
    Fixes 'Can't resolve tailwindcss-animate' error in npm workspaces.

---
+++ tailwind.config.js
@@ -0,0 +1,3 @@
+// Workaround for Tailwind IntelliSense in monorepo
+// This file helps VSCode Tailwind extension resolve the correct config
+module.exports = require('./client/tailwind.config.js');
