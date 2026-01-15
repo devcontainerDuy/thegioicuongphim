b63b4d65cd6118c4fb554d9b5b2d8d7b73e8b3cc
devcontainerDuy <khanhduytran1803@gmail.com>
2026-01-16 00:12:02 +0700

    feat: Set up initial server project with Prisma ORM and basic configuration.

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,10 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import 'dotenv/config';
+import { Injectable, Logger } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
+  private readonly logger = new Logger(PrismaService.name);
+
   constructor() {
+    if (!process.env.DATABASE_USER) {
+      throw new Error('DATABASE_USER is not defined in environment variables');
+    }
+
     const adapter = new PrismaMariaDb({
       host: process.env.DATABASE_HOST,
       port: Number(process.env.DATABASE_PORT),
